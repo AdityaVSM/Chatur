230e4f69a98cfa90392bee93fd238cc6db290df8
adityavsm-is19 <adityavsm.is19@bmsce.ac.in>
2022-12-05 20:01:06 +0530

    added basic greetings for chatbot

--- model_building/model_files/train_data.py
+++ model_building/model_files/train_data.py
@@ -12,12 +12,17 @@ from tensorflow.keras.optimizers import SGD
 
 lemmatizer = WordNetLemmatizer()
 
-intents = json.loads(open('datasets/intents.json').read())
+# nltk.download('punkt') # Uncomment if you don't have the punkt package
+# nltk.download('wordnet') # Uncomment if you don't have the wordnet package
+# nltk.download('omw-1.4') # Uncomment if you don't have the omw-1.4 package
+
+intents = json.loads(open('model_building/datasets/intents.json').read())
 
 words = []
 classes = []
 documents = []
-ignore_letters = ['?', '!', '.', ',']
+ignore_letters = ['?', '!', '.', ',', ':', ';']
+
 
 for intent in intents['intents']:
     for pattern in intent['patterns']:
@@ -26,6 +31,45 @@ for intent in intents['intents']:
         documents.append((word_list, intent['tag'], intent['response_code']))
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
-            
-print(documents)
+
+words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+
+pickle.dump(words, open('model_building/saved_models/words.pkl', 'wb'))
+pickle.dump(classes, open('model_building/saved_models/classes.pkl', 'wb'))
+
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+    
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+    
+random.shuffle(training)
+training = np.array(training, dtype=object)
+
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+history = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('model_building/saved_models/chatbot_model_v1.h5', history)
+
+print("Done")
         
