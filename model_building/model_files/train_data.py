71730a41c6ca6c333291dafd95c9e240479048a1
adityavsm-is19 <adityavsm.is19@bmsce.ac.in>
2022-12-05 18:49:24 +0530

    Built basic dataset and started model building

---
+++ model_building/model_files/train_data.py
@@ -0,0 +1,29 @@
+import random
+import numpy as np
+import json
+import pickle
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
+
+lemmatizer = WordNetLemmatizer()
+
+intents = json.loads(open('datasets/intents.json').read())
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '.', ',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag'], intent['response_code']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+        
