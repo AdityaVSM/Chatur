90287e3accad807b1df6f604231b8d384335a3b9
adityavsm-is19 <adityavsm.is19@bmsce.ac.in>
2022-12-06 19:48:36 +0530

    minor bug fix

--- pipeline.py
+++ pipeline.py
@@ -17,7 +17,7 @@ class ChatBot:
         self.dataset_path = p.dataset_path
         self.saved_model_path = p.saved_model_path
 
-    def load_models(self):
+    def __load_models(self):
         self.lemmatizer = WordNetLemmatizer()
         self.intents = json.loads(open(self.dataset_path).read())
             
@@ -26,13 +26,13 @@ class ChatBot:
 
         self.model = load_model(self.saved_model_path)
     
-    def clean_up_sentence(self,sentence):
+    def __clean_up_sentence(self,sentence):
         sentence_words = nltk.word_tokenize(sentence)
         sentence_words = [self.lemmatizer.lemmatize(word.lower()) for word in sentence_words]
         return sentence_words
 
-    def bag_of_words(self,sentence):
-        sentence_words = self.clean_up_sentence(sentence)
+    def __bag_of_words(self,sentence):
+        sentence_words = self.__clean_up_sentence(sentence)
         bag = [0] * len(self.words)
         for s in sentence_words:
             for i, word in enumerate(self.words):
@@ -40,8 +40,8 @@ class ChatBot:
                     bag[i] = 1
         return np.array(bag)
 
-    def predict_class(self,sentence):
-        bow = self.bag_of_words(sentence)
+    def __predict_class(self,sentence):
+        bow = self.__bag_of_words(sentence)
         res = self.model.predict(np.array([bow]))[0]
         ERROR_THRESHOLD = 0.25
         results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
@@ -52,7 +52,7 @@ class ChatBot:
             return_list.append({"intent": self.classes[r[0]], "probability": str(r[1])})
         return return_list
 
-    def get_response(self,ints, intents_json):
+    def __get_response(self,ints, intents_json):
         tag = ints[0]['intent']
         list_of_intents = intents_json['intents']
         for i in list_of_intents:
@@ -62,12 +62,12 @@ class ChatBot:
         return result
     
     def start_bot(self):
-        self.load_models()
+        self.__load_models()
         print("Bot is ready to chat! (type quit to stop)")
         while True:
             message = input("")
             if message.lower() == "quit":
                 break
-            ints = self.predict_class(message)
-            res = self.get_response(ints, self.intents)
+            ints = self.__predict_class(message)
+            res = self.__get_response(ints, self.intents)
             print(res)
