62d66903322c988a03f625050c0782bf2c581b27
adityavsm-is19 <adityavsm.is19@bmsce.ac.in>
2022-12-12 23:34:50 +0530

    bug fix

--- database/retrive.py
+++ database/retrive.py
@@ -4,9 +4,10 @@ import os
 
 class Retrive:
     def __init__(self):
-        self.client = client = pymongo.MongoClient("mongodb+srv://Niranjan:Niranjan15!@cluster0.lxzx5gk.mongodb.net/?retryWrites=true&w=majority")
+        self.client = pymongo.MongoClient("mongodb+srv://Niranjan:Niranjan15!@cluster0.lxzx5gk.mongodb.net/?retryWrites=true&w=majority")
         # self.client = pymongo.MongoClient("localhost",27017)      #for connecting to loacal db
         self.db = self.client.Chatur
+        
     def retrive(self,collection):
         collection = self.db[collection]
         a = collection.find_one({'Designation':"Principal"}) 
