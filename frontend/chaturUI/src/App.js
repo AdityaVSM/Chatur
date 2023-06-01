63a0f2f4712efbb9aa629bf01600a8e2c77db73e
AdityaVSM <adityavsm55@gmail.com>
2023-06-01 12:54:32 +0530

    added chatbot UI

--- frontend/chaturUI/src/App.js
+++ frontend/chaturUI/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Chatbot from './components/ChatBotContainer';
 import SearchBar from './components/searchBar';
 
 
 function App() {
   return (
-    <SearchBar/>
+    <div>
+      <div>
+        <SearchBar/>
+      </div>
+      <div>
+        <Chatbot/>
+      </div>
+    </div>
   );
 }
 export default App;
