83bced1beee376e5f943cb50c8f3547252bc6f1d
Riyo <pradeepbhatm25@gmail.com>
2023-06-08 12:17:59 +0530

    wip

--- pipeline.py
+++ pipeline.py
@@ -6,7 +6,8 @@ import  pickle
 
 import nltk
 from nltk.stem import WordNetLemmatizer
-from app_logger import logger
+import logging
+logging.basicConfig(level=logging.INFO)
 from keras.models import load_model
 
 from yaml_parser import Parser
@@ -127,7 +128,7 @@ class ChatBot:
                         # print("Email : "+  details[0]['Email'])
                         # print("Gender : "+ details[0]['Gender'])
                         # print("Designation : "+ details[0]['Designation'])     
-                        string = self.extract_info(details,ints,res_response_code)
+                        string = self.extract_info(details,ints,res_response_code, ints)
                         print(string)                   
                 else:
                     print("Can you please be more specific?")
@@ -137,18 +138,20 @@ class ChatBot:
                 print(res+"\n")
                 continue
             print("\n")
-    def extract_info(self,info,res,respo_code):
+
+    def extract_info(self,info,res,respo_code,ints):
         avoid = ['_id','id','ID number/   Aaadhaar number              (Not mandatory)','Gender','Date of joining the institution','Designation']
         stri = ''
+        resultsWithUrl = self.__get_response(ints, self.intents)
         if respo_code == 1:
             for key,value in info[0].items():
                 if key == 'url' and value ==None:
-                    urls = res
+                    urls = resultsWithUrl
                 elif key in avoid or value==None:
                     continue
                 else:
                     stri += key +':' + str(value)+'\n'
-            stri = stri + 'For more info visit : ' + res
+            stri = stri + 'For more info visit : ' + str(resultsWithUrl)
             # stri = "Name : "+  info[0]['Name']+'\n'+"Email : "+  info[0]['Email'] + "\nGender : "+ info[0]['Gender'] + "\nDesignation : "+ info[0]['Designation']
         elif respo_code ==2:
             if res[0]['intent'] == 'fee':
@@ -198,11 +201,11 @@ class ChatBot:
                     # print("Email : "+  details[0]['Email'])
                     # print("Gender : "+ details[0]['Gender'])
                     # print("Designation : "+ details[0]['Designation'])     
-                    string = self.extract_info(details,ints,res_response_code,ints)
+                    string = self.extract_info(details,ints,res_response_code, ints)
                     # print(string)
                     return string                   
             else:
-                logger.debug("unhandledQuery : ")
+                logging.info("unhandledQuery : ")
                 string = "Can you please be more specific? + 1"
                 return string
                 # print("Can you please be more specific?")
